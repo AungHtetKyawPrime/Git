99a9f105ae0dee5f57c250852aaf97dd6134bdc6
Aung Htet Kyaw <whofluni@gmail.com>
2018-10-25 15:14:44 +0630

    fourth commit

--- app/src/main/java/com/example/aspire/git/MainActivity.java
+++ app/src/main/java/com/example/aspire/git/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello aung htet Kyaw", Toast.LENGTH_SHORT).show();
+
     }
 }
